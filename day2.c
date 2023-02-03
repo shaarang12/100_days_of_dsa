766721ed04e5c33167ee4fc420ec60ac28d2c41f
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-02-03 19:43:25 +0530

    Duplicate Existence
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

---
+++ day2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdbool.h>
+// bool containsDup(int* arr, int size){
+//     for(int i = 0; i < size - 1; i ++){
+//         for(int j = i + 1; j < size; j ++){
+//             if(arr[i] == arr[j])
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// void main(){
+//     int arr[] = {1,2,3,4,1};
+//     bool ans = containsDup(arr, sizeof(arr)/4);
+//     printf(ans ? "True" : "False");
+// }
+
+struct hashTable{
+    int *
+}
+
+
+
+
+// void main(){
+//     int arr[] = {-2,-3,-1};
+//     bool ans = containsDup(arr, sizeof(arr)/4);
+//     printf(ans ? "True" : "False");
+// }
